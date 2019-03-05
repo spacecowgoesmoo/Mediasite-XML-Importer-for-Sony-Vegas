7f0b28f8920cb15ae98469f32beac85d1e4678cb
Taylor Calderone <spacecowgoesmoo@gmail.com>
2019-03-05 14:08:14 -0800

    Fixed Spacing

--- Mediasite XML Importer - 5.5.js
+++ Mediasite XML Importer - 5.5.js
@@ -32,11 +32,10 @@ fbd.Description = "Select the Mediasite folder";                                
 fbd.ShowNewFolderButton = false;                                                        // hides the 'New Folder' button
 var result = fbd.ShowDialog();                                                          // activates the browser and saves the button clicked
 var wharr = fbd.SelectedPath;                                                           // grabs the filepath that the user selects
-if (result == "Cancel")                                                                 // if the cancel button is pressed
-    {                                                                                   //
-        MessageBox.Show("Script Cancelled D:");                                         // show a message box
-		skipEverything = true;                                                          // and skip the rest of the script to prevent a crash exit
-    }                                                                                   //
+if (result == "Cancel") {                                                              	// if the cancel button is pressed
+    MessageBox.Show("Script Cancelled D:");      	                                   	// show a message box
+	skipEverything = true;                       	                                   	// and skip the rest of the script to prevent a crash exit
+}                                                	                                   	//
 
 
 
@@ -47,24 +46,24 @@ if (result == "Cancel")                                                         
 
 if (skipEverything == false){															// makes the cancel button not cause a exit crash
 	
-var blarg = new VideoTrack (1);															// create an empty track
-Vegas.Project.Tracks.Add(blarg);														// adds the empty track to the project
-var yarr : ArrayList = new ArrayList();													// initialize ArrayList
-var imageNumber = 1;																	// initialize some variables
-var r = 0;																				//
-Vegas.Project.Ruler.Format = RulerFormat.Seconds;										// change ruler format to seconds
+	var blarg = new VideoTrack (1);														// create an empty track
+	Vegas.Project.Tracks.Add(blarg);													// adds the empty track to the project
+	var yarr : ArrayList = new ArrayList();												// initialize ArrayList
+	var imageNumber = 1;																// initialize some variables
+	var r = 0;																			//
+	Vegas.Project.Ruler.Format = RulerFormat.Seconds;									// change ruler format to seconds
 
 
-var squarg: StreamReader = new StreamReader(wharr + "\\MediasitePresentation_50.xml");	// Reads an external file and executes code
-var line : String;																		// for each non-empty line of it
-while ((line = squarg.ReadLine()) != null) {											
+	var squarg: StreamReader = new StreamReader(wharr + "\\MediasitePresentation_50.xml");	// Reads an external file and executes code
+	var line : String;																		// for each non-empty line of it
+	while ((line = squarg.ReadLine()) != null) {											
 		if (line != String.Empty) {
 			if (line.substring(0, 14) == "        <Time>") {							// If a timestamp is detected in the XML file
 				yarr.Add(line.slice(14, -7));											// Store the timestamp in the array
 			}
 		}
 	}
-squarg.Close();
+	squarg.Close();
 }
 
 
@@ -87,17 +86,19 @@ for (var q in yarr) {																	// for each entry in the array
 
 	
 	if (r+1 < yarr.Count) {																// (this prevents the script from exiting with a crash)
-	var length = yarr[r+1] - yarr[r];													// calculate length of current slide
-	var time = new Timecode(yarr[r]/1000);												// convert timestamps to Vegas friendly timecode
-	length = new Timecode(length/1000);													//
-	r++;																				// increment the counter
+		var length = yarr[r+1] - yarr[r];												// calculate length of current slide
+		var time = new Timecode(yarr[r]/1000);											// convert timestamps to Vegas friendly timecode
+		length = new Timecode(length/1000);												//
+		r++;																			// increment the counter
 	} else {
-	var time = new Timecode(yarr[r]/1000);												// custom behaviour for final image
-	length = new Timecode(30000); }														// arbitrary length of 30 seconds
+		var time = new Timecode(yarr[r]/1000);											// custom behaviour for final image
+		length = new Timecode(30000);													// arbitrary length of 30 seconds
+	}																					
 	
 
 	for (var cow in Vegas.Project.Tracks) {												// Give the image the correct location & length
 		for (var moo in cow.Events) {													//
-			if (moo.Selected == true) { moo.AdjustStartLength(time, length, false); }  	//
-	} }																					//
+			if (moo.Selected == true) { moo.AdjustStartLength(time, length, false); } 	//
+		}			
+	}																					
 }
